1b5754dc8e8457e8481a12c1fee0f89cff2f2a0e
Subramanyam-1426 <nanikodiganti2006@gmail.com>
2026-09-19 17:31:35 +0530

    Time: 0 ms (100%), Space: 9.2 MB (55.86%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int dp[21][21];
+
+    bool solve(string &s, string &p, int i, int j) {
+        if (j == p.size())
+            return i == s.size();
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        bool first_match = (i < s.size() &&
+                           (s[i] == p[j] || p[j] == '.'));
+
+        if (j + 1 < p.size() && p[j + 1] == '*') {
+            return dp[i][j] = (
+                solve(s, p, i, j + 2) || 
+                (first_match && solve(s, p, i + 1, j))
+            );
+        }
+        else {
+            return dp[i][j] = 
+                (first_match && solve(s, p, i + 1, j + 1));
+        }
+    }
+
+    bool isMatch(string s, string p) {
+        memset(dp, -1, sizeof(dp));
+        return solve(s, p, 0, 0);
+    }
+};
